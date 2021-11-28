5dbb9a738d68e6e6f3071cf3756a48cd3147b630
Justin Thompson <jthomp1286@gmail.com>
2021-11-27 20:28:48 -0500

    update error page.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'ManyTwitch' });
 });
 
+router.get('/error', function(req, res, next) {
+  res.render('error', { title: 'ManyTwitch' });
+})
+
 module.exports = router;
