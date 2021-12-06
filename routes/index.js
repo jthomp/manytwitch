25ce0a8918124b380bcc250f4f6ce89a1778041f
Justin Thompson <jthomp1286@gmail.com>
2021-12-05 22:23:14 -0500

    tweaks

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const version = process.env.npm_package_version
 
 router.get('/', function(req, res) {
   res.render('index', {
-    version: version
+    layout: 'main'
   });
 });
 
