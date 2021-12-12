8e4327f307b870eef6a760900148c8e751b735b4
Justin Thompson <jthomp1286@gmail.com>
2021-12-11 19:20:36 -0500

    version bump. press enter to add a stream then another.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const version = process.env.npm_package_version
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res) {
   res.render('index', {
