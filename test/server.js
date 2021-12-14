3ee56cc335c0dcaeda539f1b9221c7e1d6ae7963
Justin Thompson <jthomp1286@gmail.com>
2021-12-14 18:55:11 -0500

    fleshing out testing tools and tests a bit.

--- test/server.js
+++ test/server.js
@@ -1,2 +1,24 @@
 var expect = require('chai').expect;
 var request = require('request');
+
+describe("Load the index", function() {
+  var url = "http://localhost:3000/";
+  
+  it("returns status 200", function(done) {
+    request(url, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+});
+
+describe("Load the index with a stream", function() {
+  var url="http://localhost:3000/arcus";
+  
+  it("returns status 200", function(done) {
+    request(url, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();      
+    });
+  });
+});
