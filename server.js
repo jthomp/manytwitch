2fb11ac4d6c5de98e255abade609049215bf5e6a
Justin Thompson <jthomp1286@gmail.com>
2022-02-02 18:41:33 -0500

    test

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/*', function(req, res) {
     version: version,
     appTitle: appTitle,
     streamsFromParms: streamsFromParms,
-    buildId: (buildId || 0)
+    buildId: (buildId.replace('v', '') || 0)
   });
 });
 
