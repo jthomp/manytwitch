9fd831982d5f25a318c65349b0198b9b2ef938b2
Justin Thompson <jthomp1286@gmail.com>
2022-02-05 22:50:18 -0500

    padding

--- public/js/manytwitch.js
+++ public/js/manytwitch.js
@@ -172,7 +172,7 @@ ManyTwitch.streams = {
       element.width = Math.floor(calculatedWidth);
     });
 
-    streamsContainer.style.paddingTop = `${containerPadding}vh`;
+    streamsContainer.style.paddingTop = `${containerPadding}px`;
 
     ManyTwitch.util.log('ManyTwitch.streams.handleResize() - End');
   },
