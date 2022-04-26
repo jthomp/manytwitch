5711d4a753a756d0f9bf6b6ccc85a7fe0b9c7bea
Justin Thompson <jthomp1286@gmail.com>
2022-04-26 12:59:03 -0400

    still playing around with tests.

--- test/app.test.js
+++ test/app.test.js
@@ -6,6 +6,9 @@ describe('Load the homepage', function() {
           .url('http://localhost:3000/')
           .assert.textContains('#default', 'Welcome to ManyTwitch')
           .assert.textContains('#default', "Let's Watch!")
+          .assert.textContains('#default', 'PSOD Notice')
+          .assert.textContains('#default', 'Created by Antillian')
+          .assert.textContains('#disclaimer', 'ManyTwitch is not affiliated with Twitch or Amazon.')
           .end();
 
   });
