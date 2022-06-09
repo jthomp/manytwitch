cacbb1f3243a94fab5307a39e7e5f7dd5d275f06
Justin Thompson <jthomp1286@gmail.com>
2022-06-09 00:00:28 -0400

    sort recents when loading.

--- public/js/manytwitch.js
+++ public/js/manytwitch.js
@@ -173,7 +173,7 @@ MT.streams = {
       recents = window.localStorage.getItem("recents");
     }
 
-    return (recents == "") ? [] : recents.sort();
+    return (recents == "") ? [] : recents.split(",").sort();
   },
 
   /**
