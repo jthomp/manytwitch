841137b78e4e3e428098aed9681047af75d2e58c
Justin Thompson <jthomp1286@gmail.com>
2022-07-02 21:00:36 -0400

    add to recents when adding.

--- public/js/manytwitch.js
+++ public/js/manytwitch.js
@@ -99,6 +99,11 @@ MT.manager = {
        saveBtn.removeAttribute("disabled");
        MT.manager.toggleAddButton();
        streamManagerDefaultContent.style.display = "none";
+
+       let currentRecentStreams = MT.streams.getRecentStreams();
+       currentRecentStreams.push(streamParm);
+       MT.streams.setRecentStreams(currentRecentStreams);
+       MT.manager.addToRecentsTable(streamParm); 
      }
  
      if (document.getElementById("streams-modal").style.display == "block") {
@@ -228,10 +233,6 @@ MT.manager = {
     if (streamParm != "") {
       log(`\t Removing stream ${streamParm}`);
       document.getElementById(`tr-${streamParm}`).remove();
-      let currentRecentStreams = MT.streams.getRecentStreams();
-      currentRecentStreams.push(streamParm);
-      MT.streams.setRecentStreams(currentRecentStreams);
-      MT.manager.addToRecentsTable(streamParm);
     }
 
     log("MT.manager.removeFromTable() - End");
