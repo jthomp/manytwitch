2dcfead3b54bf9cca65ce5f06592cce1f5783add
Justin Thompson <jthomp1286@gmail.com>
2024-03-12 22:00:26 +0000

    fix test

--- test/index.test.js
+++ test/index.test.js
@@ -1,39 +1,33 @@
-describe('Load the homepage', function() {
-
-  it('loads the index with no streams', function(browser) {
-
-	browser
-		  .url('http://localhost:8080/')
-		  .assert.visible('#default')
-		  .assert.not.visible('#streams-container')
-		  .assert.textContains('#default', 'Welcome to ManyTwitch')
-		  .assert.textContains('#default', "Let's Watch!")
-		  .assert.textContains('#default', 'By Antillian')
-		  .end();
-
+describe("Load the homepage", function () {
+  it("loads the index with no streams", function (browser) {
+    browser
+      .url("http://localhost:8080/")
+      .assert.visible("#default")
+      .assert.not.visible("#streams-container")
+      .assert.textContains("#default", "Welcome to ManyTwitch")
+      .assert.textContains("#default", "Let's Go!")
+      .assert.textContains("#default", "By Antillian")
+      .end();
   });
 
-  it('loads the index with one stream', function(browser) {
-	browser
-		  .url('http://localhost:8080/arcus')
-		  .assert.not.visible('#default')
-		  .assert.not.visible('#created')
-		  .assert.visible('#manage-btn')
-		  .assert.visible('span#stream-arcus-video')
-		  .end();
+  it("loads the index with one stream", function (browser) {
+    browser
+      .url("http://localhost:8080/arcus")
+      .assert.not.visible("#default")
+      .assert.not.visible("#created")
+      .assert.visible("#manage-btn")
+      .assert.visible("span#stream-arcus-video")
+      .end();
   });
 
-  it('loads the index with multiple streams', function(browser) {
-
-	browser
-		  .url('http://localhost:8080/arcus/lackattack')
-		  .assert.not.visible('#default')
-		  .assert.not.visible('#created')
-		  .assert.visible('#manage-btn')
-		  .assert.visible('span#stream-arcus-video')
-		  .assert.visible('span#stream-lackattack-video')
-		  .end();
-
+  it("loads the index with multiple streams", function (browser) {
+    browser
+      .url("http://localhost:8080/arcus/lackattack")
+      .assert.not.visible("#default")
+      .assert.not.visible("#created")
+      .assert.visible("#manage-btn")
+      .assert.visible("span#stream-arcus-video")
+      .assert.visible("span#stream-lackattack-video")
+      .end();
   });
-
 });
