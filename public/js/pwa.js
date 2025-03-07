74b93346738c40880c8493326096d41f08ad2cab
Justin Thompson <jthomp1286@gmail.com>
2025-03-07 02:25:03 -0500

    console.log => console.debug

--- public/js/pwa.js
+++ public/js/pwa.js
@@ -3,9 +3,9 @@ function init() {
 	if ("serviceWorker" in navigator) {
 		navigator.serviceWorker.register("/js/sw.js")
 	  	.then((reg) => {
-			console.log("Service worker registered -->", reg);
+			console.debug("Service worker registered -->", reg);
 	  	}, (err) => {
-			console.error("Service worker not registered -->", err);
+			console.debug("Service worker not registered -->", err);
 	  	});
   	}
 }
